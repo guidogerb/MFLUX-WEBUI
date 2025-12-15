6a45f8ea04d3539a4b039cc352d1a65daaba91ff
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-12-15 03:06:08 +0100

    fix regex

--- start.js
+++ start.js
@@ -11,7 +11,7 @@ module.exports = {
           "python webui.py",
         ],
         on: [{
-          "event": "/http:\/\/\\S+/",   
+          "event": "/http:\\/\\/127\\.0\\.0\\.1:\\d{2,5}$/",   
           "done": true
         }, {
           "event": "/error:/i",
